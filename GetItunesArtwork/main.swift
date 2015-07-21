5fe02c8f54e1f63e66715d36729ab793b008bc18
Michael Skiba <mike.skiba@raizlabs.com>
2015-07-20 22:48:35 -0400

    Initial Commit

---
+++ GetItunesArtwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GetItunesArtwork
+//
+//  Created by Michael Skiba on 7/20/15.
+//  Copyright © 2015 AtelierClockwork. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
